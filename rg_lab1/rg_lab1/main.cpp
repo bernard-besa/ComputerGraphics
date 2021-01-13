9d696d799ef885b4738d18f7c6826df5e69a5f8e
bernard-besa <bernard.kukic@fer.hr>
2021-01-13 17:35:23 +0100

    Added rg_lab1 and rg_lab2

---
+++ rg_lab1/rg_lab1/main.cpp
@@ -0,0 +1,306 @@
+﻿#include <cstdio>
+#include <glm.hpp>
+#include <gtc/matrix_access.hpp>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <GL/glut.h>
+using namespace std;
+
+//*********************************************************************************
+//  Razredi potrebni za iscrtavanje tijela.
+//  Vertex - predstavlja vrh/toèku u prostoru s koordinatama x, y, z
+//  Polygon - predstavlja poligon u prostoru s vrhovima v1, v2, v3
+//  Trajectory - predstavlja putanju po kojoj se tijelo animira
+//*********************************************************************************
+
+
+std::vector<glm::vec3> BSplineVertices;
+
+glm::mat4 B = glm::mat4(
+    glm::vec4(-1, 3, -3, 1),
+    glm::vec4(3, -6, 0, 4),
+    glm::vec4(-3, 3, 3, 1),
+    glm::vec4(1, 0, 0, 0));
+glm::mat4x3 dB = glm::mat4x3(
+    glm::vec3(-1, 2, -1),
+    glm::vec3(3, -4, 0),
+    glm::vec3(-3, 2, 1),
+    glm::vec3(1, 0, 0));
+glm::mat4x2 ddB = glm::mat4x2(
+    glm::vec2(-1, 1),
+    glm::vec2(3, -2),
+    glm::vec2(-3, 1),
+    glm::vec2(1, 0));
+
+glm::vec3 calculateP(double t, int i) {
+    glm::vec4 T = glm::vec4(pow(t, 3), pow(t, 2), t, 1);
+    glm::mat3x4 R = glm::mat3x4(
+        glm::vec4(BSplineVertices[i - 1].x, BSplineVertices[i].x, BSplineVertices[i + 1].x, BSplineVertices[i + 2].x),
+        glm::vec4(BSplineVertices[i - 1].y, BSplineVertices[i].y, BSplineVertices[i + 1].y, BSplineVertices[i + 2].y),
+        glm::vec4(BSplineVertices[i - 1].z, BSplineVertices[i].z, BSplineVertices[i + 1].z, BSplineVertices[i + 2].z));
+    glm::vec3 p = T * 0.1666666f * B * R;
+    return p;
+}
+
+glm::vec3 calculateDiffP(double t, int i) {
+    glm::vec3 T = glm::vec3(pow(t, 2), t, 1);
+    glm::mat3x4 R = glm::mat3x4(
+        glm::vec4(BSplineVertices[i - 1].x, BSplineVertices[i].x, BSplineVertices[i + 1].x, BSplineVertices[i + 2].x),
+        glm::vec4(BSplineVertices[i - 1].y, BSplineVertices[i].y, BSplineVertices[i + 1].y, BSplineVertices[i + 2].y),
+        glm::vec4(BSplineVertices[i - 1].z, BSplineVertices[i].z, BSplineVertices[i + 1].z, BSplineVertices[i + 2].z));
+    glm::vec3 diffP = T * 0.5f * dB * R;
+    return diffP;
+}
+
+glm::vec3 calculateDoubleDiffP(double t, int i) {
+    glm::vec2 T = glm::vec2(t, 1);
+    glm::mat3x4 R = glm::mat3x4(
+        glm::vec4(BSplineVertices[i - 1].x, BSplineVertices[i].x, BSplineVertices[i + 1].x, BSplineVertices[i + 2].x),
+        glm::vec4(BSplineVertices[i - 1].y, BSplineVertices[i].y, BSplineVertices[i + 1].y, BSplineVertices[i + 2].y),
+        glm::vec4(BSplineVertices[i - 1].z, BSplineVertices[i].z, BSplineVertices[i + 1].z, BSplineVertices[i + 2].z));
+    glm::vec3 doubleDiffP = T * ddB * R;
+    return doubleDiffP;
+}
+
+vector<glm::vec3> vertices;
+vector<vector<glm::vec3>> polygons;
+glm::vec3 center;
+double t = 0.0, clock = 0.0, previousClock = 0.0;
+int speed = 1, i = 1;
+bool is_stopped = false, usingDCM = true;
+
+GLuint window;
+GLuint width = 700, height = 500;
+
+void myDisplay();
+void myIdle();
+void myKeyboard(unsigned char theKey, int mouseX, int mouseY);
+void loadObject(string filePath);
+void loadBSpline(string filePath);
+void drawObject(glm::mat3 R);
+void drawBSpline();
+
+int main(int argc, char** argv) {
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(100, 100);
+    glutInit(&argc, argv);
+    window = glutCreateWindow("RG lab1");
+    //ako se koristi DCM
+    if (usingDCM)
+        gluLookAt(0, 0.5, 1, 0, 20, 50, 0, 5, 5);
+    else
+    {
+        glMatrixMode(GL_PROJECTION);
+    }
+    loadObject("../objFiles/skull.obj");
+    loadBSpline("../BSpline2.txt");
+
+    glutDisplayFunc(myDisplay);
+    glutKeyboardFunc(myKeyboard);
+    glutIdleFunc(myIdle);
+    glutMainLoop();
+    return 0;
+}
+
+void myIdle() {
+    if (is_stopped)
+    {
+        return;
+    }
+    clock += 0.01f;
+    double timePassed = clock - previousClock;
+    if (timePassed > speed)
+    {
+        myDisplay();
+        clock = 0;
+    }
+}
+
+void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(1.0, 1.0, 1.0, 1);
+
+    //racunanje tangente
+    glColor3f(0.0, 0.0, 1.0);
+    glm::vec3 tangent = calculateDiffP(t, i);
+    tangent = glm::normalize(tangent);
+    glm::vec3 tangentStart = calculateP(t, i);
+    glm::vec3 tangentEnd = tangentStart + 5.f * tangent;
+    glBegin(GL_LINES);
+    glVertex3f(tangentStart.x / 25.f, tangentStart.y / 25.f, tangentStart.z / 25.f);
+    glVertex3f(tangentEnd.x / 25.f, tangentEnd.y / 25.f, tangentEnd.z / 25.f);
+    glEnd();
+
+    //racunanje normale
+    glColor3f(0.0, 1.0, 0.0);
+    glm::vec3 ddp = calculateDoubleDiffP(t, i);
+    glm::vec3 normal = glm::normalize(glm::cross(tangent, ddp));
+    glm::vec3 normalStart = calculateP(t, i);
+    glm::vec3 normalEnd = normalStart + 5.f * normal;
+    glBegin(GL_LINES);
+    glVertex3f(normalStart.x / 25.f, normalStart.y / 25.f, normalStart.z / 25.f);
+    glVertex3f(normalEnd.x / 25.f, normalEnd.y / 25.f, normalEnd.z / 25.f);
+    glEnd();
+
+    //racunanje binormale
+    glColor3f(1.0, 0.0, 1.0);
+    glm::vec3 binormal = glm::normalize(glm::cross(tangent, normal));
+    glm::vec3 binormalStart = calculateP(t, i);
+    glm::vec3 binormalEnd = binormalStart + 5.f * binormal;
+    glBegin(GL_LINES);
+    glVertex3f(binormalStart.x / 25.f, binormalStart.y / 25.f, binormalStart.z / 25.f);
+    glVertex3f(binormalEnd.x / 25.f, binormalEnd.y / 25.f, binormalEnd.z / 25.f);
+    glEnd();
+
+    //rotacijska matrica (DCM)
+    glm::mat3 rotationMatrix = glm::mat3(tangent, normal, binormal);
+
+    drawBSpline();
+    drawObject(rotationMatrix);
+
+    t += 0.1;
+    if (t >= 1.)
+    {
+        i++;
+        t = 0.1;
+    }
+
+    if (i >= BSplineVertices.size() - 2)
+    {
+        i = 1;
+        t = 0.;
+    }
+    glFlush();
+}
+
+void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
+    switch (theKey)
+    {
+    case 27:  exit(0);
+        break;
+    }
+}
+
+void loadBSpline(string filename) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        double x, y, z;
+        istringstream iss(line);
+        iss >> x >> y >> z;
+        glm::vec3 vertex(x, y, z);
+        BSplineVertices.push_back(vertex);
+    }
+}
+
+void loadObject(string fileName) {
+    ifstream file(fileName);
+    string line;
+    center = glm::vec3(0, 0, 0);
+    while (getline(file, line))
+    {
+        //vrh
+        if (line.compare(0, 1, "v") == 0)
+        {
+            double x, y, z;
+            char c;
+            istringstream iss(line);
+            iss >> c >> x >> y >> z;
+            glm::vec3 vertex(x, y, z);
+            vertices.push_back(vertex);
+        }
+        //poligon
+        else if (line.compare(0, 1, "f") == 0)
+        {
+            int indexV1, indexV2, indexV3;
+            char c;
+            istringstream iss(line);
+            iss >> c >> indexV1 >> indexV2 >> indexV3;
+            glm::vec3 vertex1 = vertices[indexV1 - 1];
+            glm::vec3 vertex2 = vertices[indexV2 - 1];
+            glm::vec3 vertex3 = vertices[indexV3 - 1];
+
+            center += vertex1 + vertex2 + vertex3;
+
+            glm::mat4 rotation = glm::mat4(
+                glm::vec4(cos(-80), 0, sin(-80), 0),
+                glm::vec4(0, 1, 0, 0),
+                glm::vec4(-sin(-80), 0, cos(-80), 0),
+                glm::vec4(0, 0, 0, 1));
+            glm::vec4 p1 = glm::vec4(vertex1.x, vertex1.y, vertex1.z, 1) * rotation;
+            glm::vec4 p2 = glm::vec4(vertex2.x, vertex2.y, vertex2.z, 1) * rotation;
+            glm::vec4 p3 = glm::vec4(vertex3.x, vertex3.y, vertex3.z, 1) * rotation;
+
+            vertex1 = glm::vec3(p1.x, p1.y, p1.z);
+            vertex2 = glm::vec3(p2.x, p2.y, p2.z);
+            vertex3 = glm::vec3(p3.x, p3.y, p3.z);
+            std::vector<glm::vec3> poly;
+            poly.push_back(vertex1);
+            poly.push_back(vertex2);
+            poly.push_back(vertex3);
+            polygons.push_back(poly);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    center /= polygons.size();
+}
+
+void drawBSpline() {
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+    for (auto& point : BSplineVertices) {
+        glVertex3f(point.x / 25.f, point.y / 25.f, point.z / 25.f);
+    }
+    glEnd();
+}
+
+void drawObject(glm::mat3 R) {
+    if (!usingDCM)
+    {
+        glm::vec3 tangent = glm::normalize(glm::column(R, 0));
+        glm::vec3 start = glm::normalize(glm::vec3(0, 0.89, 0));
+        glm::vec3 axis = glm::normalize(glm::cross(start, tangent));
+        double fi = acos(glm::dot(start, tangent));
+        glRotatef((fi * 180) / (atan(1) * 4), axis.x, axis.y, axis.z);
+    }
+
+    glColor3f(1.0, 1.0, 0.0);
+    glBegin(GL_TRIANGLES);
+    for (auto& p : polygons)
+    {
+        glm::vec3 position = calculateP(t, i);
+        glm::vec3 position_vec = glm::vec3(position.x, position.y, position.z);
+
+        glm::vec3 v1 = p[0];
+        glm::vec3 v1_vec = glm::vec3(v1.x, v1.y, v1.z);
+        glm::vec3 v1_final = (v1_vec - center) / 5.f + position_vec / 25.f;
+        if (usingDCM)
+            v1_final = ((v1_vec - center) * glm::inverse(R)) / 5.f + position_vec / 25.f;
+
+        glm::vec3 v2 = p[1];
+        glm::vec3 v2_vec = glm::vec3(v2.x, v2.y, v2.z);
+        glm::vec3 v2_final = (v2_vec - center) / 5.f + position_vec / 25.f;
+        if (usingDCM)
+            v2_final = ((v2_vec - center) * glm::inverse(R)) / 5.f + position_vec / 25.f;
+
+        glm::vec3 v3 = p[2];
+        glm::vec3 v3_vec = glm::vec3(v3.x, v3.y, v3.z);
+        glm::vec3 v3_final = (v3_vec - center) / 5.f + position_vec / 25.f;
+        if (usingDCM)
+            v3_final = ((v3_vec - center) * glm::inverse(R)) / 5.f + position_vec / 25.f;
+
+        glVertex3f(v1_final.x, v1_final.y, v1_final.z);
+        glVertex3f(v2_final.x, v2_final.y, v2_final.z);
+        glVertex3f(v3_final.x, v3_final.y, v3_final.z);
+    }
+    glEnd();
+}
---
+++ rg_lab2/rg_lab2/Source.cpp
@@ -0,0 +1,540 @@
+﻿#include <cstdio>
+#include <glm.hpp>
+#include <gtc/matrix_access.hpp>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <GL/glut.h>
+#include <algorithm>
+#include <ctime>
+using namespace std;
+
+
+class Material
+{
+public:
+    Material(string _name) : name(_name) {}
+
+    string name;
+    glm::vec3 Kd;
+    glm::vec3 Ks;
+    glm::vec3 Ka;
+    double d;
+};
+
+class MyObject
+{
+public:
+    MyObject(string _name) : name(_name) {}
+
+    string name;
+    vector<vector<glm::vec3>> polygons;
+    Material* material;
+    glm::vec3 color;
+};
+
+
+class Particle
+{
+public:
+    Particle(glm::vec3 _position, glm::vec3 _color, double _size) : position(_position), color(_color), size(_size)
+    {
+        isGrey = false;
+        age = 1.0;
+        angle = 0;
+        axis = glm::vec3(0.0, 0.0, 0.0);
+    }
+
+    void draw()
+    {
+        glColor3f(color.x, color.y, color.z);
+        glPushMatrix();
+        glTranslatef(position.x, position.y, position.z);
+        glRotatef(angle, axis.x, axis.y, axis.z);
+        glutSolidSphere(size, 5, 5);
+        glPopMatrix();
+    }
+
+    glm::vec3 position;
+    glm::vec3 color;
+    glm::vec3 axis;
+    double angle;
+    double size;
+    double age;
+    bool isGrey;
+};
+
+vector<glm::vec2> textures;
+vector<glm::vec3> vertices;
+vector<glm::vec3> normals;
+vector<vector<glm::vec3>> polygons;
+vector<MyObject> objects;
+vector<Material *> materials;
+vector<Particle> particles;
+glm::vec3 ociste = glm::vec3(0.0, 4.7, 20.0);
+glm::vec3 lightSource = glm::vec3(-4.6, 15.2, 12.0);
+glm::vec3 lightColor = glm::vec3(1.0, 1.0, 1.0);
+glm::vec3 ambientLightColor = glm::vec3(0.5, 0.5, 0.5);
+glm::vec3 particleStartingPosition = glm::vec3(0.0, -7.0, -2.0);
+double clockNow = 0.0, previousClock = 0.0;
+
+GLuint window;
+GLuint width = 700, height = 500;
+
+void myDisplay();
+void myIdle();
+void myReshape(int width, int height);
+void updatePerspective();
+void myKeyboard(unsigned char theKey, int mouseX, int mouseY);
+void loadScene(string filePath);
+void loadMaterial(string filePath);
+void drawScene();
+void drawParticles();
+void particleControl();
+double clamp(double x, double lower, double upper);
+glm::vec3 calculateLighting(glm::vec3 kd, glm::vec3 ks, glm::vec3 centroid, glm::vec3 point, double n);
+
+
+int main(int argc, char** argv) {
+    
+    srand(time(NULL));
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("RG lab2");
+    
+    loadMaterial("../objFiles/fireplace.mtl");
+    loadScene("../objFiles/fireplace.obj");
+    glutReshapeFunc(myReshape);
+    glutDisplayFunc(myDisplay);
+    glutKeyboardFunc(myKeyboard);
+    glutIdleFunc(myIdle);
+
+    glEnable(GL_CULL_FACE);
+
+    glutMainLoop();
+    return 0;
+}
+
+void myIdle() {
+    int current_time = glutGet(GLUT_ELAPSED_TIME);
+    int diff = current_time - previousClock;
+    if (diff > 60)
+    {
+        particleControl();
+        myDisplay();
+        previousClock = current_time;
+    }
+}
+
+void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.6, 0.6, 0.8, 1);
+    drawScene();
+    drawParticles();
+    glutSwapBuffers();
+}
+
+void myReshape(int w, int h)
+{
+    width = w;
+    height = h;
+    glViewport(0, 0, width, height);
+    updatePerspective();
+}
+
+void updatePerspective()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (float)width / height, 0.5, 150.0); 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(ociste.x, ociste.y, ociste.z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // O x,y,z; glediste x,y,z; up vektor x,y,z
+}
+
+void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
+    switch (theKey)
+    {
+    case 27:  exit(0);
+        break;
+    case 'd':
+        ociste.x += 2.0;
+        break;
+    case 'a':
+        ociste.x -= 2.0;
+        break;
+    case 's':
+        ociste.z -= 2.0;
+        break;
+    case 'w':
+        ociste.z += 2.0;
+        break;
+    }
+    updatePerspective();
+    glutPostRedisplay();
+}
+
+void loadScene(string fileName) {
+    ifstream file(fileName);
+    string line, previousLine = "NULL";
+    while (true)
+    {
+        if (line.compare(0, 1, "") == 0 && previousLine.compare(0, 1, "o") == 0)
+        {
+            break;
+        }
+        if (previousLine.compare("NULL") == 0)
+        {
+            if (!getline(file, line))
+            {
+                break;
+            }
+        }
+        else
+        {
+            line = previousLine;
+        }
+        //objekt
+        if (line.compare(0, 1, "o") == 0)
+        {
+            
+            string name;
+            char c;
+            istringstream iss(line);
+            vector<vector<glm::vec3>> polys;
+            iss >> c >> name;
+            MyObject object(name);
+            while (getline(file, line))
+            {
+                //normala vrha
+                if (line.compare(0, 2, "vn") == 0)
+                {
+                    double x, y, z;
+                    string vn;
+                    istringstream iss(line);
+                    iss >> vn >> x >> y >> z;
+                    normals.push_back(glm::vec3(x, y, z));
+                }
+                //textura vrha
+                else if (line.compare(0, 2, "vt") == 0)
+                {
+                    double x, y;
+                    string vt;
+                    istringstream iss(line);
+                    iss >> vt >> x >> y;
+                    textures.push_back(glm::vec2(x, y));
+                }
+                //vrh
+                else if (line.compare(0, 1, "v") == 0)
+                {
+                    double x, y, z;
+                    char c;
+                    istringstream iss(line);
+                    iss >> c >> x >> y >> z;
+                    vertices.push_back(glm::vec3(x, y, z));
+                }
+                //poligon
+                else if (line.compare(0, 1, "f") == 0)
+                {
+                    string firstIndex, secondIndex, thirdIndex, fourthIndex;
+                    char c;
+                    istringstream iss(line);
+                    vector<glm::vec3> poly;
+                    iss >> c >> firstIndex >> secondIndex >> thirdIndex >> fourthIndex;
+                    
+                    int index1 = stoi(firstIndex.substr(0, firstIndex.find("/"))) - 1;
+                    int index2 = stoi(secondIndex.substr(0, secondIndex.find("/"))) - 1;
+                    int index3 = stoi(thirdIndex.substr(0, thirdIndex.find("/"))) - 1;
+                    poly.push_back(vertices[index1]);
+                    poly.push_back(vertices[index2]);
+                    poly.push_back(vertices[index3]);
+                    if (!fourthIndex._Equal("")) {
+                        int index4 = stoi(fourthIndex.substr(0, fourthIndex.find("/"))) - 1;
+                        poly.push_back(vertices[index4]);
+                    }
+                    polygons.push_back(poly);
+                    polys.push_back(poly);
+                }
+                //naziv materijala
+                else if (line.compare(0, 6, "usemtl") == 0)
+                {
+                    string name;
+                    string usemtl;
+                    istringstream iss(line);
+                    iss >> usemtl >> name;
+                    auto it = find_if(materials.begin(), materials.end(), [&name](const Material* obj) { return obj->name == name; });
+                    if (it != materials.end())
+                    {
+                        auto index = it - materials.begin();
+                        object.material = materials[index];
+                    }
+                }
+                //novi objekt
+                else if (line.compare(0, 1, "o") == 0)
+                {
+                    previousLine = line;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            object.color = glm::vec3((double)rand() / RAND_MAX, (double)rand() / RAND_MAX, (double)rand() / RAND_MAX);
+            object.polygons = polys;
+            objects.push_back(object);
+        }
+    }
+}
+
+void drawParticles() {
+    for (auto& particle : particles) {
+        particle.draw();
+    }
+}
+
+void loadMaterial(string fileName)
+{
+    ifstream file(fileName);
+    string line;
+    while (getline(file, line))
+    {
+        //novi materijal
+        if (line.compare(0, 6, "newmtl") == 0)
+        {
+            string name;
+            string newmtl;
+            istringstream iss(line);
+            iss >> newmtl >> name;
+            Material* material = new Material(name);
+            while (getline(file, line))
+            {
+                //ambijentalna komponenta
+                if (line.compare(0, 2, "Ka") == 0)
+                {
+                    double x, y, z;
+                    string Ka;
+                    istringstream iss(line);
+                    iss >> Ka >> x >> y >> z;
+                    glm::vec3 ka(x, y, z);
+                    material->Ka = ka;
+                }
+                //difuzna komponenta
+                else if (line.compare(0, 2, "Kd") == 0)
+                {
+                    double x, y, z;
+                    string Kd;
+                    istringstream iss(line);
+                    iss >> Kd >> x >> y >> z;
+                    glm::vec3 kd(x, y, z);
+                    material->Kd = kd;
+                }
+                //spekularna komponenta
+                else if (line.compare(0, 2, "Ks") == 0)
+                {
+                    double x, y, z;
+                    string Ks;
+                    istringstream iss(line);
+                    iss >> Ks >> x >> y >> z;
+                    glm::vec3 ks(x, y, z);
+                    material->Ks = ks;
+                }
+                //indeks hrapavosti
+                else if (line.compare(0, 1, "d") == 0)
+                {
+                    double n;
+                    char d;
+                    istringstream iss(line);
+                    iss >> d >> n;
+                    material->d = n;
+                }
+                //kraj
+                else if (line.compare(0, 5, "illum") == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            materials.push_back(material);
+        }
+    }
+}
+
+
+void drawScene() {
+
+    for (auto& object : objects)
+    {
+        glColor3f(object.color.x, object.color.y, object.color.z);
+        //if (object.name.find("Text") != string::npos) continue;
+        for (auto& poly : object.polygons)
+        {
+            
+            /*
+            // Ambijentno (Ia * ka)
+            glm::vec3 ka = object.material->Ka;
+            double red = ambientLightColor.x * ka.x;
+            double green = ambientLightColor.y * ka.y;
+            double blue = ambientLightColor.z * ka.z;
+            glm::vec3 ambient = glm::vec3(red, green, blue);
+            double centroidx = 0.0, centroidy = 0.0, centroidz = 0.0;
+            for (auto& point : poly) {
+                centroidx += point.x;
+            }
+            for (auto& point : poly) {
+                centroidy += point.y;
+            }
+            for (auto& point : poly) {
+                centroidz += point.z;
+            }
+            
+            glm::vec3 centroid = glm::vec3(centroidx / poly.size(), centroidy / poly.size(), centroidz / poly.size());
+            glm::vec3 kd = object.material->Kd;
+            glm::vec3 ks = object.material->Ks;
+            double n = object.material->d;
+            
+            */
+            
+            if (poly.size() == 4) {
+                glBegin(GL_POLYGON);
+            }
+            else {
+                glBegin(GL_TRIANGLES);
+            }
+            
+            for (auto& point : poly) {
+                
+                //glm::vec3 diffuse = calculateLighting(kd, ks, centroid, point, n);
+                //glm::vec3 color = ambient + diffuse;
+                //glColor3f(clamp(color.x, 0.0, 1.0), clamp(color.y, 0.0, 1.0), clamp(color.z, 0.0, 1.0));
+                glVertex3f(point.x, point.y, point.z);
+            }
+            glEnd();
+        }
+        
+    }
+}
+
+glm::vec3 calculateLighting(glm::vec3 kd, glm::vec3 ks, glm::vec3 centroid, glm::vec3 point, double n) {
+        glm::vec3 color = glm::vec3(0.0, 0.0, 0.0);
+
+        glm::vec3 L = glm::vec3(lightSource.x - point.x, lightSource.y - point.y, lightSource.z - point.z);
+        L = glm::normalize(L);
+        glm::vec3 N = glm::vec3(point.x - centroid.x, point.y - centroid.y, point.z - centroid.z);
+        N = glm::normalize(N);
+        double LN = glm::dot(L, N);
+
+        // Difuzno (Ii * kd * (L*N))
+        double red = lightColor.x * kd.x * LN;
+        double green = lightColor.y * kd.y * LN;
+        double blue = lightColor.z * kd.z * LN;
+        color = color + glm::vec3(red, green, blue);
+
+        // R = N * (2 * L*N) - L
+        glm::vec3 R = glm::vec3(2 * LN * N.x, 2 * LN * N.y, 2 * LN * N.z) - L;
+        R = glm::normalize(R);
+        glm::vec3 V = glm::vec3(ociste.x - point.x, ociste.y - point.y, ociste.z - point.z);
+        V = glm::normalize(V);
+        double RV = glm::dot(R, V);
+
+        // Spekularno (Ii * ks * (R*V)^n)
+        red = lightColor.x * ks.x * pow(RV, n);
+        green = lightColor.y * ks.y * pow(RV, n);
+        blue = lightColor.z * ks.z * pow(RV, n);
+        color = color + glm::vec3(red, green, blue);
+
+        return color;
+}
+
+double clamp(double x, double lower, double upper)
+{
+    return min(upper, max(x, lower));
+}
+
+void particleControl()
+{
+    //nove
+    glm::vec3 position = particleStartingPosition;
+    glm::vec3 color = glm::vec3(1.0, 1.0, 0.0);
+    double size = 0.07;
+    Particle p1(position, color, size);
+    Particle p2(position, color, size);
+    Particle p3(position, color, size);
+    Particle p4(position, color, size);
+    Particle p5(position, color, size);
+    Particle p6(position, color, size);
+    particles.push_back(p1);
+    particles.push_back(p2);
+    particles.push_back(p3);
+    particles.push_back(p4);
+    particles.push_back(p5);
+    particles.push_back(p6);
+    for (int i = particles.size() - 1; i >= 0; i--)
+    {
+        //pomak
+        glm::vec3 current_position = particles.at(i).position;
+        double xTranslation = -(double)rand() / RAND_MAX * 0.3 + (double)rand() / RAND_MAX * 0.3;
+        double zTranslation = -(double)rand() / RAND_MAX * 0.2 + (double)rand() / RAND_MAX * 0.2;
+        particles.at(i).position = glm::vec3(current_position.x + xTranslation, clamp(current_position.y + 0.01, particleStartingPosition.y, particleStartingPosition.y + 6.0), current_position.z + zTranslation);
+
+        //rotacija
+        double xComponent = 0.0 + (double)rand() / RAND_MAX;
+        double yComponent = 0.0 + (double)rand() / RAND_MAX;
+        double zComponent = 0.0 + (double)rand() / RAND_MAX;
+        particles.at(i).axis = glm::vec3(xComponent, yComponent, zComponent);
+        double rotationAngle = 0.0 + (double)rand() / RAND_MAX * 360.0;
+        particles.at(i).angle = rotationAngle;
+        
+        //boja
+        glm::vec3 newPosition = particles.at(i).position;
+        if (abs(abs(newPosition.x) - abs(particleStartingPosition.x)) >= 1.5 || abs(abs(newPosition.z) - abs(particleStartingPosition.z)) >= 1.5)
+        {
+            //tamnije sivo
+            if (particles.at(i).isGrey)
+            {
+                glm::vec3 currentColor = particles.at(i).color;
+                particles.at(i).color = glm::vec3(currentColor.x - 0.04, currentColor.x - 0.04, currentColor.x - 0.04);
+            }
+            //postavi u sivo
+            else
+            {
+                particles.at(i).isGrey = true;
+                particles.at(i).color = glm::vec3(0.3689, 0.3689, 0.3689);
+            }
+            //unisti
+            if (abs(abs(newPosition.x) - abs(particleStartingPosition.x)) >= 1.7 || abs(abs(newPosition.z) - abs(particleStartingPosition.z)) >= 1.7)
+            {
+                particles.erase(particles.begin() + i);
+                continue;
+            }
+        }
+        //potamniti boju
+        else
+        {
+            //u crno
+            if (particles.at(i).isGrey)
+            {
+                particles.at(i).color = glm::vec3(0.0, 0.0, 0.0);
+            }
+            glm::vec3 currentColor = particles.at(i).color;
+            //prema crvenoj
+            particles.at(i).color = glm::vec3(currentColor.x, clamp(currentColor.y - 0.01, 0.0, 1.0), currentColor.z);
+        }
+        
+        //unisti zbog vremena
+        particles.at(i).age -= 0.005;
+        if (particles.at(i).age <= 0.0)
+        {
+            particles.erase(particles.begin() + i);
+        }
+        
+    }
+}
